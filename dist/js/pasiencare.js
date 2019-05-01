110d4a6d679e0d657ae5449d91820267008d689f
Bagus Arisandi <bagusarisandi@Baguss-MacBook-Pro.local>
2019-05-01 11:41:32 +0700

    move js script

--- dist/js/pasiencare.js
+++ dist/js/pasiencare.js
@@ -28,7 +28,22 @@ $(document).ready(function () {
         $('#sidebar').addClass('active');
         $('.overlay').addClass('active');
     });
+
+
+    $('#inputGroupFile02').on('change',function(){
+        //get the file name
+        var fileName = $(this).val();
+        //replace the "Choose a file" label
+        $(this).next('.custom-file-label').html(fileName);
+    })
 });
+
+function trainingStatementNo(){
+    document.getElementById('trainingForm').style.display = 'none';
+}
+function trainingStatementYes(){
+    document.getElementById('trainingForm').style.display = 'block';
+}
 
 
 var scrollPixel = 0;
